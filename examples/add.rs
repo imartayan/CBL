a8dda42520ab607d2c66920c05c9e76281a7ef9c
Igor Martayan <igor@martayan.org>
2023-12-18 13:15:01 +0100

    Update examples

--- examples/add.rs
+++ examples/add.rs
@@ -1,11 +1,9 @@
-use cbl::bit_container::*;
-use cbl::container::*;
+use cbl::kmer::*;
 use cbl::reads::*;
 use cbl::CBL;
 use clap::Parser;
 use core::cmp::min;
 use std::time::Instant;
-// use cbl::kmer::*;
 
 // Loads runtime-provided constants for which declarations
 // will be generated at `$OUT_DIR/constants.rs`.
@@ -13,9 +11,7 @@ pub mod constants {
     include!(concat!(env!("OUT_DIR"), "/constants.rs"));
 }
 
-use constants::{K, KMER_BITS, KT, M, POS_BITS};
-const N: usize = KMER_BITS; // canonize?
-const W: usize = K - M + 1;
+use constants::{K, KT};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -24,30 +20,12 @@ struct Args {
     input: String,
 }
 
-type PrefixContainer = RankBitContainer;
-// type SuffixContainer = PlainVec<u32>;
-type SuffixContainer = SemiSortedVec<u32, 32>;
-
 fn main() {
     let args = Args::parse();
     let input_filename = args.input.as_str();
 
-    let mut cbl = CBL::<N, POS_BITS, W, K, KT, PrefixContainer, SuffixContainer>::new();
+    let mut cbl = CBL::<K, KT, u32>::new();
     let reads = Fasta::from_file(input_filename);
-
-    // reads.process(|nucs| {
-    //     let mut kmer = RawKmer::<K, KT>::new();
-    //     for (i, base) in nucs.filter_map(KT::from_nuc).enumerate() {
-    //         if i < K - 1 {
-    //             kmer = kmer.extend(base as KT);
-    //         } else {
-    //             kmer = kmer.append(base as KT);
-    //             cbl.insert(kmer);
-    //             // assert!(cbl.contains(kmer));
-    //             // debug_assert!(cbl.contains(kmer));
-    //         }
-    //     }
-    // });
 
     reads.process_rec(|rec| {
         let seq = rec.seq();
@@ -63,20 +41,24 @@ fn main() {
 
         println!("prefix load: {}", cbl.prefix_load());
         println!("suffix load: {}", cbl.suffix_load());
-        // for (size, proportion) in cbl.suffix_load_repartition() {
-        //     let percent = (proportion * 100.0).round();
-        //     if percent > 0.0 {
-        //         println!("size {size}: {percent} %");
-        //     }
-        // }
+        for (size, proportion) in cbl.suffix_load_repartition() {
+            let percent = (proportion * 100.0).round();
+            if percent > 0.0 {
+                println!("size {size}: {percent} %");
+            }
+        }
 
-        // let mut _res = true;
-        // let now = Instant::now();
-        // for start in (0..(seq.len() - K + 1)).step_by(chunk_size) {
-        //     _res ^= cbl.contains_seq(&seq[start..(min(start + chunk_size + K - 1, seq.len()))]);
-        // }
-        // let elapsed = now.elapsed().as_nanos();
-        // println!("{} ns/membership", elapsed / n);
+        let mut _res = true;
+        let now = Instant::now();
+        for start in (0..(seq.len() - K + 1)).step_by(chunk_size) {
+            for nucs in seq[start..(min(start + chunk_size + K - 1, seq.len()))].windows(K) {
+                let kmer = RawKmer::<K, KT>::from_nucs(nucs);
+                _res ^= cbl.contains(kmer);
+            }
+            // _res ^= cbl.contains_seq(&seq[start..(min(start + chunk_size + K - 1, seq.len()))]);
+        }
+        let elapsed = now.elapsed().as_nanos();
+        println!("{} ns/membership", elapsed / n);
 
         let now = Instant::now();
         for start in (0..(seq.len() - K + 1)).step_by(chunk_size) {
