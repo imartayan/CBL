99f3e5fce2818412425b87275fee0796cfd671c1
Igor Martayan <igor@martayan.org>
2023-12-22 15:20:09 +0100

    Remove useless test

--- src/kmer.rs
+++ src/kmer.rs
@@ -59,13 +59,6 @@ pub trait Kmer<const K: usize, T: Base>: Sized + Copy + RevComp + Ord + Hash {
     #[inline]
     fn from_bases_iter<I: Iterator<Item = T>>(bases: I) -> Self {
         bases.take(K).fold(Self::new(), |s, base| s.extend(base))
-        // Self::from_int(
-        //     bases
-        //         .take(K)
-        //         .enumerate()
-        //         .map(|(i, base)| base << (2 * (K - 1 - i)))
-        //         .sum(),
-        // )
     }
     #[inline]
     fn from_bases(bases: &[T]) -> Self {
@@ -290,19 +283,6 @@ impl<const K: usize> RevComp for RawKmer<K, u128> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_kmer_from_nucs() {
-        const K: usize = 31;
-        let seq = [b'G'; 1000];
-        let mut _res = 0;
-        for _ in 0..100000 {
-            for nucs in seq.windows(31) {
-                let kmer = RawKmer::<K, u64>::from_nucs(nucs);
-                _res ^= kmer.to_int();
-            }
-        }
-        assert_eq!(_res, 0);
-    }
     #[test]
     fn test_rc_8() {
         let kmer = RawKmer::<4, u8>::from_nucs(b"ATCG");
