21e47e2f801c02229526de9f518ebfe3c324b444
Igor Martayan <igor@martayan.org>
2024-01-13 17:07:11 +0100

    Remove compression

--- src/container/mod.rs
+++ src/container/mod.rs
@@ -1,11 +1,7 @@
-mod compressed_vec;
-mod compressor;
 mod plain_vec;
 mod semi_sorted_vec;
 
 use crate::compact_int::CompactInt;
-pub use compressed_vec::SemiCompressed;
-pub use compressor::{Compressor, SnapCompressor};
 pub use plain_vec::PlainVec;
 pub use semi_sorted_vec::SemiSortedVec;
 
@@ -28,33 +24,6 @@ pub trait Container<T> {
     fn iter<'a>(&'a self) -> impl ExactSizeIterator<Item = &'a T>
     where
         T: 'a;
-}
-
-pub trait CompressedContainer<const BYTES: usize, CompressorT: Compressor> {
-    fn new() -> Self;
-    fn new_with_one(compressor: &mut CompressorT, x: CompactInt<BYTES>) -> Self;
-    fn len(&self, compressor: &CompressorT) -> usize;
-    #[inline]
-    fn is_empty(&self, compressor: &CompressorT) -> bool {
-        self.len(compressor) == 0
-    }
-    fn contains(&self, compressor: &mut CompressorT, x: &CompactInt<BYTES>) -> bool;
-    fn insert(&mut self, compressor: &mut CompressorT, x: CompactInt<BYTES>) -> bool;
-    fn remove(&mut self, compressor: &mut CompressorT, x: &CompactInt<BYTES>) -> bool;
-    fn insert_iter<I: ExactSizeIterator<Item = CompactInt<BYTES>>>(
-        &mut self,
-        compressor: &mut CompressorT,
-        it: I,
-    );
-    fn remove_iter<I: ExactSizeIterator<Item = CompactInt<BYTES>>>(
-        &mut self,
-        compressor: &mut CompressorT,
-        it: I,
-    );
-    // fn iter<'a>(
-    //     &'a self,
-    //     compressor: &mut CompressorT,
-    // ) -> impl ExactSizeIterator<Item = &'a CompactInt<BYTES>>;
 }
 
 #[cfg(test)]
@@ -100,75 +69,6 @@ mod tests {
         assert!(container.is_empty());
     }
 
-    fn test_compressed_container<
-        const BYTES: usize,
-        CompressorT: Compressor,
-        CompressedContainerT: CompressedContainer<BYTES, CompressorT>,
-    >() {
-        let mut container = CompressedContainerT::new();
-        let mut compressor = Compressor::new();
-        let v0 = (0..(2 * N)).step_by(2).collect_vec();
-        let v1 = (0..(2 * N)).skip(1).step_by(2).collect_vec();
-        for &i in v0.iter() {
-            assert!(
-                container.insert(&mut compressor, CompactInt::from_int(i)),
-                "failed to insert {i}"
-            );
-        }
-        for &i in v0.iter() {
-            assert!(
-                container.contains(&mut compressor, &CompactInt::from_int(i)),
-                "false negative {i}"
-            );
-        }
-        for &i in v1.iter() {
-            assert!(
-                !container.contains(&mut compressor, &CompactInt::from_int(i)),
-                "false positive {i}"
-            );
-        }
-        for &i in v0.iter() {
-            assert_eq!(container.len(&compressor), N - i / 2, "wrong len");
-            assert!(
-                container.remove(&mut compressor, &CompactInt::from_int(i)),
-                "failed to remove {i}"
-            );
-        }
-        assert!(container.is_empty(&compressor));
-    }
-
-    fn test_compressed_container_iter<
-        const BYTES: usize,
-        CompressorT: Compressor,
-        CompressedContainerT: CompressedContainer<BYTES, CompressorT>,
-    >() {
-        let mut container = CompressedContainerT::new();
-        let mut compressor = Compressor::new();
-        let v0 = (0..(2 * N)).step_by(2).collect_vec();
-        let v1 = (0..(2 * N)).skip(1).step_by(2).collect_vec();
-        container.insert_iter(
-            &mut compressor,
-            v0.iter().copied().map(CompactInt::from_int),
-        );
-        for &i in v0.iter() {
-            assert!(
-                container.contains(&mut compressor, &CompactInt::from_int(i)),
-                "false negative {i}"
-            );
-        }
-        for &i in v1.iter() {
-            assert!(
-                !container.contains(&mut compressor, &CompactInt::from_int(i)),
-                "false positive {i}"
-            );
-        }
-        container.remove_iter(
-            &mut compressor,
-            v0.iter().copied().map(CompactInt::from_int),
-        );
-        assert!(container.is_empty(&compressor));
-    }
-
     #[test]
     fn test_plain_vec() {
         for _ in 0..10000 {
@@ -182,22 +82,6 @@ mod tests {
         for _ in 0..10000 {
             test_container::<SemiSortedVec<T, 32>>();
             test_container_iter::<SemiSortedVec<T, 32>>();
-        }
-    }
-
-    #[test]
-    fn test_compressed_vec() {
-        for _ in 0..10000 {
-            test_compressed_container::<
-                2,
-                SnapCompressor,
-                SemiCompressed<2, 64, SemiSortedVec<CompactInt<2>, 32>, SnapCompressor>,
-            >();
-            test_compressed_container_iter::<
-                2,
-                SnapCompressor,
-                SemiCompressed<2, 64, SemiSortedVec<CompactInt<2>, 32>, SnapCompressor>,
-            >();
         }
     }
 }
--- src/wordset/mod.rs
+++ src/wordset/mod.rs
@@ -1,7 +1,5 @@
 mod bit_wordset;
-mod compressed_wordset;
 mod hash_wordset;
 
 pub use bit_wordset::*;
-pub use compressed_wordset::*;
 pub use hash_wordset::*;
