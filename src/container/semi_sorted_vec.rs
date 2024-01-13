5756a124f5b5215f88883083538245edb9dbd2da
Igor Martayan <igor@martayan.org>
2024-01-13 16:52:49 +0100

    Implement set operations for SemiSortedVec

--- src/container/semi_sorted_vec.rs
+++ src/container/semi_sorted_vec.rs
@@ -1,4 +1,7 @@
 use super::Container;
+use core::cmp::Ordering;
+use core::ops::*;
+use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
 pub struct SemiSortedVec<T: Ord, const THRESHOLD: usize> {
@@ -101,5 +104,268 @@ impl<T: Ord, const THRESHOLD: usize> Container<T> for SemiSortedVec<T, THRESHOLD
         T: 'a,
     {
         self.vec.iter()
+    }
+}
+
+impl<T: Copy + Ord, const THRESHOLD: usize> BitOr<Self> for &mut SemiSortedVec<T, THRESHOLD> {
+    type Output = SemiSortedVec<T, THRESHOLD>;
+
+    fn bitor(self, other: Self) -> Self::Output {
+        let mut vec = Vec::new();
+        self.vec.sort_unstable();
+        other.vec.sort_unstable();
+        let mut self_iter = self.vec.iter();
+        let mut other_iter = other.vec.iter();
+        let mut x = self_iter.next();
+        let mut y = other_iter.next();
+        while let (Some(a), Some(b)) = (x, y) {
+            match a.cmp(b) {
+                Ordering::Less => {
+                    vec.push(*a);
+                    x = self_iter.next();
+                }
+                Ordering::Greater => {
+                    vec.push(*b);
+                    y = other_iter.next();
+                }
+                Ordering::Equal => {
+                    vec.push(*a);
+                    x = self_iter.next();
+                    y = other_iter.next();
+                }
+            }
+        }
+        while let Some(a) = x {
+            vec.push(*a);
+            x = self_iter.next();
+        }
+        while let Some(b) = y {
+            vec.push(*b);
+            y = other_iter.next();
+        }
+        unsafe { Self::Output::from_vec_unchecked(vec) }
+    }
+}
+
+impl<T: Copy + Ord, const THRESHOLD: usize> BitAnd<Self> for &mut SemiSortedVec<T, THRESHOLD> {
+    type Output = SemiSortedVec<T, THRESHOLD>;
+
+    fn bitand(self, other: Self) -> Self::Output {
+        let mut vec = Vec::new();
+        self.vec.sort_unstable();
+        other.vec.sort_unstable();
+        let mut self_iter = self.vec.iter();
+        let mut other_iter = other.vec.iter();
+        let mut x = self_iter.next();
+        let mut y = other_iter.next();
+        while let (Some(a), Some(b)) = (x, y) {
+            match a.cmp(b) {
+                Ordering::Less => {
+                    x = self_iter.next();
+                }
+                Ordering::Greater => {
+                    y = other_iter.next();
+                }
+                Ordering::Equal => {
+                    vec.push(*a);
+                    x = self_iter.next();
+                    y = other_iter.next();
+                }
+            }
+        }
+        unsafe { Self::Output::from_vec_unchecked(vec) }
+    }
+}
+
+impl<T: Copy + Ord, const THRESHOLD: usize> Sub<Self> for &mut SemiSortedVec<T, THRESHOLD> {
+    type Output = SemiSortedVec<T, THRESHOLD>;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let mut vec = Vec::new();
+        self.vec.sort_unstable();
+        other.vec.sort_unstable();
+        let mut self_iter = self.vec.iter();
+        let mut other_iter = other.vec.iter();
+        let mut x = self_iter.next();
+        let mut y = other_iter.next();
+        while let (Some(a), Some(b)) = (x, y) {
+            match a.cmp(b) {
+                Ordering::Less => {
+                    vec.push(*a);
+                    x = self_iter.next();
+                }
+                Ordering::Greater => {
+                    y = other_iter.next();
+                }
+                Ordering::Equal => {
+                    x = self_iter.next();
+                    y = other_iter.next();
+                }
+            }
+        }
+        while let Some(a) = x {
+            vec.push(*a);
+            x = self_iter.next();
+        }
+        unsafe { Self::Output::from_vec_unchecked(vec) }
+    }
+}
+
+impl<T: Copy + Ord, const THRESHOLD: usize> BitXor<Self> for &mut SemiSortedVec<T, THRESHOLD> {
+    type Output = SemiSortedVec<T, THRESHOLD>;
+
+    fn bitxor(self, other: Self) -> Self::Output {
+        let mut vec = Vec::new();
+        self.vec.sort_unstable();
+        other.vec.sort_unstable();
+        let mut self_iter = self.vec.iter();
+        let mut other_iter = other.vec.iter();
+        let mut x = self_iter.next();
+        let mut y = other_iter.next();
+        while let (Some(a), Some(b)) = (x, y) {
+            match a.cmp(b) {
+                Ordering::Less => {
+                    vec.push(*a);
+                    x = self_iter.next();
+                }
+                Ordering::Greater => {
+                    vec.push(*b);
+                    y = other_iter.next();
+                }
+                Ordering::Equal => {
+                    x = self_iter.next();
+                    y = other_iter.next();
+                }
+            }
+        }
+        while let Some(a) = x {
+            vec.push(*a);
+            x = self_iter.next();
+        }
+        while let Some(b) = y {
+            vec.push(*b);
+            y = other_iter.next();
+        }
+        unsafe { Self::Output::from_vec_unchecked(vec) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::Itertools;
+
+    const N: usize = 1000;
+
+    #[test]
+    fn test_union() {
+        let mut container = SemiSortedVec::<usize, 32>::new();
+        let mut container2 = SemiSortedVec::<usize, 32>::new();
+        let v0 = (0..(3 * N)).step_by(3).collect_vec();
+        let v1 = (0..(3 * N)).skip(1).step_by(3).collect_vec();
+        let v2 = (0..(3 * N)).skip(2).step_by(3).collect_vec();
+        for &i in v0.iter() {
+            container.insert(i);
+        }
+        for &i in v1.iter() {
+            container2.insert(i);
+        }
+        container = &mut container | &mut container2;
+        for i in v0.iter() {
+            assert!(container.contains(i), "false negative for {i}");
+        }
+        for i in v1.iter() {
+            assert!(container.contains(i), "false negative for {i}");
+        }
+        for i in v2.iter() {
+            assert!(!container.contains(i), "false positive for {i}");
+        }
+    }
+
+    #[test]
+    fn test_intersection() {
+        let mut container = SemiSortedVec::<usize, 32>::new();
+        let mut container2 = SemiSortedVec::<usize, 32>::new();
+        let v0 = (0..(3 * N)).step_by(3).collect_vec();
+        let v1 = (0..(3 * N)).skip(1).step_by(3).collect_vec();
+        let v2 = (0..(3 * N)).skip(2).step_by(3).collect_vec();
+        for &i in v0.iter() {
+            container.insert(i);
+        }
+        for &i in v1.iter() {
+            container.insert(i);
+            container2.insert(i);
+        }
+        for &i in v2.iter() {
+            container2.insert(i);
+        }
+        container = &mut container & &mut container2;
+        for i in v0.iter() {
+            assert!(!container.contains(i), "false positive for {i}");
+        }
+        for i in v1.iter() {
+            assert!(container.contains(i), "false negative for {i}");
+        }
+        for i in v2.iter() {
+            assert!(!container.contains(i), "false positive for {i}");
+        }
+    }
+
+    #[test]
+    fn test_difference() {
+        let mut container = SemiSortedVec::<usize, 32>::new();
+        let mut container2 = SemiSortedVec::<usize, 32>::new();
+        let v0 = (0..(3 * N)).step_by(3).collect_vec();
+        let v1 = (0..(3 * N)).skip(1).step_by(3).collect_vec();
+        let v2 = (0..(3 * N)).skip(2).step_by(3).collect_vec();
+        for &i in v0.iter() {
+            container.insert(i);
+        }
+        for &i in v1.iter() {
+            container.insert(i);
+            container2.insert(i);
+        }
+        for &i in v2.iter() {
+            container2.insert(i);
+        }
+        container = &mut container - &mut container2;
+        for i in v0.iter() {
+            assert!(container.contains(i), "false negative for {i}");
+        }
+        for i in v1.iter() {
+            assert!(!container.contains(i), "false positive for {i}");
+        }
+        for i in v2.iter() {
+            assert!(!container.contains(i), "false positive for {i}");
+        }
+    }
+
+    #[test]
+    fn test_symmetric_difference() {
+        let mut container = SemiSortedVec::<usize, 32>::new();
+        let mut container2 = SemiSortedVec::<usize, 32>::new();
+        let v0 = (0..(3 * N)).step_by(3).collect_vec();
+        let v1 = (0..(3 * N)).skip(1).step_by(3).collect_vec();
+        let v2 = (0..(3 * N)).skip(2).step_by(3).collect_vec();
+        for &i in v0.iter() {
+            container.insert(i);
+        }
+        for &i in v1.iter() {
+            container.insert(i);
+            container2.insert(i);
+        }
+        for &i in v2.iter() {
+            container2.insert(i);
+        }
+        container = &mut container ^ &mut container2;
+        for i in v0.iter() {
+            assert!(container.contains(i), "false negative for {i}");
+        }
+        for i in v1.iter() {
+            assert!(!container.contains(i), "false positive for {i}");
+        }
+        for i in v2.iter() {
+            assert!(container.contains(i), "false negative for {i}");
+        }
     }
 }
