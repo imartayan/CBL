309c604ceb7003711f64ad5885ab7dc0e0647874
Igor Martayan <igor@martayan.org>
2024-01-18 20:02:28 +0100

    Remove KmerNecklaceFactory

--- src/necklace/mod.rs
+++ src/necklace/mod.rs
@@ -4,7 +4,6 @@ pub(crate) mod minimizer;
 mod queue;
 pub mod rank;
 
-use crate::kmer::{Base, Kmer};
 use num_traits::int::PrimInt;
 pub use queue::NecklaceQueue;
 
@@ -26,59 +25,6 @@ pub fn necklace_pos<const BITS: usize, T: PrimInt>(word: T) -> (T, usize) {
 pub fn revert_necklace_pos<const BITS: usize, T: PrimInt>(necklace: T, pos: usize) -> T {
     ((necklace << (BITS - pos)) & ((T::one() << BITS) - T::one())) | (necklace >> pos)
 }
-
-#[derive(Debug)]
-pub struct KmerNecklaceFactory<const K: usize, const BITS: usize, T: Base, const M: usize = 9>
-where
-    [(); BITS - M + 1]:,
-{
-    necklace_queue: NecklaceQueue<BITS, T, { BITS - M + 1 }>,
-}
-
-macro_rules! impl_necklace_factory {
-($($T:ty),+) => {$(
-impl<const K: usize, const BITS: usize, const M: usize> KmerNecklaceFactory<K, BITS, $T, M>
-where
-    [(); BITS - M + 1]:,
-{
-    pub fn new() -> Self {
-        Self {
-            necklace_queue: NecklaceQueue::<BITS, $T, { BITS - M + 1 }>::new(),
-        }
-    }
-
-    pub fn from_kmer<KmerT: Kmer<K, $T>>(&self, kmer: KmerT) -> ($T, usize) {
-        necklace_pos::<BITS, $T>(kmer.to_int())
-    }
-
-    pub fn from_bases(&mut self, bases: &[$T]) -> Vec<($T, usize)> {
-        let mut res = Vec::with_capacity(bases.len() - K + 1);
-        let mut word = 0;
-        for &base in &bases[..K] {
-            word = (word << 2) | base;
-        }
-        self.necklace_queue.insert_full(word);
-        res.push(self.necklace_queue.get_necklace_pos());
-        for &base in &bases[K..] {
-            self.necklace_queue.insert2(base);
-            res.push(self.necklace_queue.get_necklace_pos());
-        }
-        res
-    }
-}
-
-impl<const K: usize, const BITS: usize, const M: usize> Default
-    for KmerNecklaceFactory<K, BITS, $T, M>
-where
-    [(); BITS - M + 1]:,
-{
-    fn default() -> Self {
-        Self::new()
-    }
-}
-)*}}
-
-impl_necklace_factory!(u8, u16, u32, u64, u128);
 
 #[cfg(test)]
 mod tests {
