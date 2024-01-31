537296eff8e6add07c0c1b9b4ab7f24c395b3553
Igor Martayan <igor@martayan.org>
2024-01-31 13:59:11 +0100

    Fix typos

--- src/necklace/mod.rs
+++ src/necklace/mod.rs
@@ -8,7 +8,7 @@ pub mod rank;
 use num_traits::int::PrimInt;
 pub use queue::NecklaceQueue;
 
-/// Return the necklace (smallest cyclic rotation) of a word and its position.
+/// Returns the necklace (smallest cyclic rotation) of a word and its position.
 pub fn necklace_pos<const BITS: usize, T: PrimInt>(word: T) -> (T, usize) {
     let mut necklace = word;
     let mut rot = word;
@@ -23,7 +23,7 @@ pub fn necklace_pos<const BITS: usize, T: PrimInt>(word: T) -> (T, usize) {
     (necklace, pos)
 }
 
-/// Recover a word from its necklace and position.
+/// Recovers a word from its necklace and position.
 #[inline]
 pub fn revert_necklace_pos<const BITS: usize, T: PrimInt>(necklace: T, pos: usize) -> T {
     ((necklace << (BITS - pos)) & ((T::one() << BITS) - T::one())) | (necklace >> pos)
